aada439017dace0090212a7f19b73766fa4f40ec
SLedolter <s.ledolter@gmail.com>
2022-05-23 20:14:19 +0200

    SL: encapsulated gui into own class; fixed menu and readLine/readChar

---
+++ EnigmaMachine/ConsoleGUI.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace EnigmaMachine {
+  class ConsoleGUI {
+    const int COMMAND_AREA_HEIGHT = 4;
+    const string MENU_ICON = ">>";
+
+    private string lastInput;
+    private bool userWantsToExit;
+    private string currentMenuShortname = "";
+
+    public string LastInput { get => lastInput; set => lastInput = value; }
+    public bool UserWantsToExit { get => userWantsToExit; set => userWantsToExit = value; }
+
+    public ConsoleGUI() { }
+
+    public void ShowStartScreenAndGetInput() {
+      lastInput = "";
+      lastInput = ShowPromptAndReadCommandLine();
+    }
+
+    public string ShowPromptAndReadCommandLine(string menuPrefix = "") {
+      currentMenuShortname = CreateMenuPrefix(menuPrefix);
+
+      Console.Clear();
+      DrawCommandSectionAndPlaceInputCursor();
+      lastInput = ReadLine();
+      RunUserCommand();
+
+      return lastInput;
+    }
+
+    public ConsoleKey ShowPromptAndReadCommandChar(string menuPrefix = "") {
+      ConsoleKey command;
+      currentMenuShortname = CreateMenuPrefix(menuPrefix);
+
+      Console.Clear();
+      DrawCommandSectionAndPlaceInputCursor();
+      command = ReadChar();
+
+      return command;
+    }
+
+    public string CreateMenuPrefix(string menuPrefixIn) {
+      string result;
+
+      if(menuPrefixIn.Length > 0) {
+        result = menuPrefixIn + " ";
+      } else {
+        result = "";
+      }
+
+      return result;
+    }
+
+    private void PlaceInputCursorToPromptSign() {
+      Console.CursorLeft = currentMenuShortname.Length + MENU_ICON.Length + 1;
+      Console.CursorTop = Console.WindowHeight - COMMAND_AREA_HEIGHT + 2;
+    }
+
+    private string ReadLine() {
+      PlaceInputCursorToPromptSign();
+      return Console.ReadLine();
+    }
+
+    private ConsoleKey ReadChar() {
+      PlaceInputCursorToPromptSign();
+      return Console.ReadKey().Key ;
+    }
+
+    private void DrawCommandSectionAndPlaceInputCursor() {
+      Console.CursorTop = Console.WindowHeight - COMMAND_AREA_HEIGHT;
+      for (int i = 0; i < Console.WindowWidth; i++) {
+        Console.CursorLeft = i;
+        Console.Write("-");
+      }
+      Console.CursorLeft = 0;
+      Console.CursorTop = Console.WindowHeight - COMMAND_AREA_HEIGHT + 2;
+      Console.Write($"{currentMenuShortname}>> ");
+    }
+
+    private void RunUserCommand() {
+      switch (lastInput) {
+        case "1":
+          EncodeLetters();
+          break;
+        case "exit":
+          userWantsToExit = true;
+          break;
+      }
+    }
+
+    public void EncodeLetters() {
+      ConsoleKey input;
+      
+      do {
+        input = ShowPromptAndReadCommandChar("Enc");
+      } while (input != ConsoleKey.OemMinus);
+    }
+  }
+}
--- EnigmaMachine/Program.cs
+++ EnigmaMachine/Program.cs
@@ -4,60 +4,25 @@ namespace EnigmaMachine
 {
   class Program
   {
-    public static EnigmaMachine enigmaMachine;
-
-    const int COMMAND_AREA_HEIGHT = 4;
+    private static EnigmaMachine enigmaMachine;
+    private static ConsoleGUI consoleGUI;
 
     static void Main(string[] args)
     {
-      string userInput;
-      
       enigmaMachine = new EnigmaMachine("MyEnigmaMachine");
-      
+      consoleGUI = new ConsoleGUI();
+
       do {
         Console.Clear();
-        ShowUserInterface(enigmaMachine);
-        userInput = ShowAndReadCommandPrompt();
-        RunUserCommand(userInput);
-      } while (userInput != "exit");
+        consoleGUI.ShowStartScreenAndGetInput();
+        consoleGUI.ShowStartScreenAndGetInput();
+      } while (!consoleGUI.UserWantsToExit);
     }
 
     static void ShowUserInterface(EnigmaMachine enigmaMachine) {
       Console.CursorLeft = 0;
       Console.CursorTop = 0;
       Console.Error.WriteLine($"Name: {enigmaMachine.Name}");
-    }
-
-    static string ShowAndReadCommandPrompt() {
-      string command;
-
-      Console.CursorTop = Console.WindowHeight - COMMAND_AREA_HEIGHT;
-      for (int i = 0; i < Console.WindowWidth; i++) {
-        Console.CursorLeft = i;
-        Console.Write("-");
-      }
-
-      Console.CursorLeft = 0;
-      Console.CursorTop = Console.WindowHeight - COMMAND_AREA_HEIGHT + 2;
-      Console.Write(">> ");
-      command = Console.ReadLine();
-      return command;
-    }
-
-    static void RunUserCommand(string userInput) {
-      switch (userInput) {
-        case "1":
-          EncodeLetters();
-          break;
-      }
-    }
-
-    static void EncodeLetters() {
-      ConsoleKey input;
-
-      do {
-        input = Console.ReadKey().Key;
-      } while (input != ConsoleKey.OemMinus);
     }
   }
 }
